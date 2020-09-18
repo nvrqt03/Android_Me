9e1cce38205bbc6f21b9a0b686eeebf9dfa003b8
nvrqt03 <ajktg21@gmail.com>
2020-09-18 12:35:38 -0500

    added MasterListFragment and implemented onImageClickListener

--- app/src/main/java/ajmitchell/android/android_me/ui/MainActivity.java
+++ app/src/main/java/ajmitchell/android/android_me/ui/MainActivity.java
@@ -1,6 +1,7 @@
 package ajmitchell.android.android_me.ui;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,10 +9,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import ajmitchell.android.android_me.R;
 // todo's in notion notes for Master List Fragment
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MasterListFragment.OnImageClickListener {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onImageSelected(int position) {
+        Toast.makeText(this, "Postion clicked = " + position, Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/ajmitchell/android/android_me/ui/MasterListFragment.java
+++ app/src/main/java/ajmitchell/android/android_me/ui/MasterListFragment.java
@@ -1,9 +1,11 @@
 package ajmitchell.android.android_me.ui;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.GridView;
 
 import androidx.annotation.NonNull;
@@ -14,6 +16,27 @@ import ajmitchell.android.android_me.R;
 import ajmitchell.android.android_me.data.AndroidImageAssets;
 
 public class MasterListFragment extends Fragment {
+    // listener that triggers callback in host activity
+    OnImageClickListener mCallback;
+
+    // interface to be implemented by MainActivity - specifying the method it triggers. takes position of the image the user clicks in our gridView
+    public interface OnImageClickListener {
+        void onImageSelected(int position);
+    }
+
+    //make sure the host activity implements the callback by checking onAttach
+
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        try {
+            mCallback = (OnImageClickListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString()
+                + " must implement OnImageClickListenr");
+        }
+    }
 
     // mandatory empty constructor
     public MasterListFragment() {
@@ -32,9 +55,18 @@ public class MasterListFragment extends Fragment {
 
         // create MasterList Adapter
         // this adapter takes in the context and an arrayList of all the image resources to display
-        MasterListAdapter mAdapter = new MasterListAdapter(getContext(), AndroidImageAssets.getAll());
+        final MasterListAdapter mAdapter = new MasterListAdapter(getContext(), AndroidImageAssets.getAll());
 
         gridView.setAdapter(mAdapter);
+
+        // attach listener to gridView
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                // Trigger the callback method and pass in the position that was clicked
+                mCallback.onImageSelected(position);
+            }
+        });
 
         // return the rootView
         return rootView;
